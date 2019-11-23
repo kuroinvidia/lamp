48fb92ace2c2e36a66779677182d64faf9ca3622
kuroinvidia <57119803+kuroinvidia@users.noreply.github.com>
2019-11-23 13:23:27 +0700

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# https://github.com/kuroinvidia
+
 apt install apache2 php7.0 php7.0-mysql mysql-server bind9 ssh -y
 echo -e " \n Y \n admin \n admin \n Y \n Y \n Y \n Y" | mysql_secure_installation #Skills39 adalah password database untuk root user, ganti aja
 systemctl restart apache2
@@ -8,8 +10,8 @@ cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/ph
 sed -i 's+/var/www/html+/usr/share/phpmyadmin+g' /etc/apache2/sites-available/phpmyadmin.conf
 a2ensite phpmyadmin.conf
 systemctl restart apache2
+cp latest.tar.gz /var/www/html
 cd /var/www/html
-wget https://wordpress.org/latest.tar.gz
 tar -zxvf latest.tar.gz
 cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/wordpress.conf
 sed -i 's+/var/www/html+/var/www/html/wordpress+g' /etc/apache2/sites-available/wordpress.conf
